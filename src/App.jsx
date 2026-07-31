b437150689bce64dc27f91aeaa18334ed17cdace
Sam McNair <Sam.McNair@mcnairs.dev>
2026-07-31 20:27:04 +0100

    Test render of App component to "status: Green"

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,12 @@
 import './App.css'
-import Hero from './components/Hero/Hero'
-import SearchForm from './components/SearchForm/SearchForm'
-import SearchResults from './components/SearchResults/SearchResults'
-import PlaylistForm from './components/PlaylistForm/PlaylistForm'
-import Playlist from './components/Playlist/Playlist'
-
+/* import Hero from './components/Hero/Hero' 
+      <Hero />
+*/
 function App() {
-//  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Jammming</h1>
-        <section id="hero">
-          <h3>Hero Section</h3>
-          <Hero />
-        </section>
-        <section id="search">
-          <h3>Search Section</h3>
-          <section id="search-form">
-            <h4>SearchForm Component</h4>
-            <SearchForm />
-          </section>
-          <section id="search-results">
-            <h4>SearchResults Component</h4>
-            <SearchResults />
-          </section>
-        </section>
-        <section id="playlist">
-          <h3>Playlist Section</h3>
-          <section id="playlist-form">
-            <h4>PlaylistForm Component</h4>
-            <PlaylistForm />
-          </section>
-          <section id="playlist-tracks">
-            <h4>Playlist Component</h4>
-            <Playlist />
-          </section>
-        </section>
     </>
   )
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,34 +2,11 @@
 import { render, screen } from '@testing-library/react'
 import { describe, it, expect } from 'vitest'
 import App from './App'
-import Hero from './components/Hero/Hero'
-import SearchForm from './components/SearchForm/SearchForm'
-import SearchResults from './components/SearchResults/SearchResults'
-import PlaylistForm from './components/PlaylistForm/PlaylistForm'
-import Playlist from './components/Playlist/Playlist'
 
-describe('Jammming web app and immediate children', () => {
-  it('renders the App component with child components', () => {
+describe('Jammming web app', () => {
+  it('renders the App component with title', () => {
     render(<App />)
-    })
 
-  it('renders Hero component with CTA', () => {
-    render(<Hero />)
-  })
-  
-  it('renders SearchForm component', () => {
-    render(<SearchForm />)
-  })
-  
-  it('renders SearchResults component', () => {
-    render(<SearchResults />)
-  })
-  
-  it('renders PlaylistForm component', () => {
-    render(<PlaylistForm />)
-  })
-  
-  it('renders Playlist component', () => {
-    render(<Playlist />)
+    expect(screen.getByText('Jammming')).toBeInTheDocument()
   })
 })
