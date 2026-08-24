72db5cbc1ea7b6be89fa024ce23544e6ab69c22d
Sam McNair <Sam.McNair@mcnairs.dev>
2026-08-24 12:07:24 +0100

    fix: typo in html code for PlaylistForm component

--- src/components/PlaylistForm/PlaylistForm.jsx
+++ src/components/PlaylistForm/PlaylistForm.jsx
@@ -5,7 +5,7 @@ function PlaylistForm({id}) {
     <form id={id}>
       <label htmlFor="playlist-name">Playlist Name... (required):</label>
       <input type="text" name="playlist-name" id="playlist-name" required />
-      <label htmlFor="playlist-descritption">Playlist Description... (optional):</label>
+      <label htmlFor="playlist-description">Playlist Description... (optional):</label>
       <textarea name="playlist-description" id="playlist-description"></textarea>
       <button type="submit">Save to Spotify</button>            
     </form>
